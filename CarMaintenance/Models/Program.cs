4deae21772889ff6f7e425694f0baf22e4d15856
kevin <syy2000t@gmail.com>
2023-05-02 01:52:24 +0800

    資料庫連線字串改放appsettings

---
+++ CarMaintenance/Models/Program.cs
@@ -0,0 +1,31 @@
+using CarMaintenance;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<RepairContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("RepairContext")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
